01b15c8d39ac2d42a7ed69634f00c43eeb169cc0
Valentin THUILLIER <valentin.thuillier@luxferre-code.fr>
2024-09-25 18:35:47 +0200

    Modify skills & adding base projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    NgIf
+  ],
   templateUrl: './projects.component.html',
-  styleUrl: './projects.component.scss'
+  styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent {
+  selectedProject: any = null;
 
+  projects = [
+    {
+      title: 'Développement d\'une API REST 🍕',
+      description: 'Projet réalisé en 2ème année à l\'IUT de Lille. Le but était de développer une API REST pour une pizzeria 🏪. L\'API permet de gérer les ingrédients, les pizzas, et les commandes, facilitant ainsi les opérations de la pizzeria.',
+      technologies: ['API REST', 'JavaEE', 'PostgreSQL', 'SQL', 'Rédaction de rapport'],
+      image: 'assets/projects/api-rest.png',
+      link: 'https://github.com/vthuillier/S4.A02.1-Api_REST'
+    },
+    {
+      title: 'UniCo | Affectation d\'étudiants 👨‍🎓👩‍🎓',
+      description: 'Projet en équipe de 3, réalisé pour affecter des étudiants à des séjours linguistiques 🗣️ en fonction de critères comme les allergies et passe-temps. Nous avons utilisé des algorithmes de graphes pour les associations optimales.',
+      technologies: ['Java', 'Graphes', 'JavaFX', 'CSV'],
+      image: 'assets/projects/unico.png',
+      link: 'https://github.com/vthuillier/UniCo'
+    },
+    {
+      title: 'Chasse au Monstre 👾',
+      description: 'Projet de jeu réalisé à l\'IUT de Lille. Le but était de concevoir et développer un jeu vidéo 🎮 avec une interface graphique en Java. Nous avons travaillé en groupe pour créer le diagramme de cas d\'utilisation et de classes avant d\'implémenter les fonctionnalités du jeu.',
+      technologies: ['Java', 'IHM', 'Algorithmes IA', 'JavaFX', 'Algorithme A*'],
+      image: 'assets/projects/chasse-au-monstre.png',
+      link: 'https://github.com/vthuillier/ChasseAuMonstre'
+    },
+    {
+      title: 'Space Invaders ++ 🚀',
+      description: 'Projet en équipe de 2 : développement d\'un jeu Shoot\'Em Up inspiré de Space Invaders en TypeScript 👾. Nous avons intégré un système de score, de vies, de niveaux, et de difficulté, ainsi qu\'un mode multijoueur via Socket.io.',
+      technologies: ['TypeScript', 'HTML/CSS', 'Socket.io', 'Git', 'Node.js'],
+      image: 'assets/projects/space-invaders.png',
+      link: 'https://github.com/vthuillier/SpaceInvadersPlusPlus'
+    },
+    {
+      title: 'E-Portfolio en Angular 🌐',
+      description: 'Création de mon portfolio personnel 💼 en Angular, avec des animations et un mode dark/light pour une expérience utilisateur personnalisée.',
+      technologies: ['Angular', 'TypeScript', 'SCSS', 'Bootstrap'],
+      image: 'assets/projects/portfolio-angular.jpg',
+      link: 'https://github.com/vthuillier/MyPortfolio'
+    }
+  ];
+
+  openProject(project: any) {
+    this.selectedProject = project;
+  }
+
+  closeModal() {
+    this.selectedProject = null;
+  }
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -9,27 +9,29 @@ import {NgForOf, NgStyle} from "@angular/common";
     NgForOf
   ],
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'] // Corrigez le 'styleUrls'
 })
 export class SkillsComponent {
+  selectedSkill: any = null;  // Pour suivre la compétence sélectionnée
+
   skills = [
-    { name: 'Java', level: 90, image: 'java' },
-    { name: 'Python', level: 85, image: 'python' },
-    { name: 'SQL', level: 80, image: 'sql' },
-    { name: 'JavaEE', level: 75, image: 'javaee' },
-    { name: 'PHP', level: 70, image: 'php' },
-    { name: 'Premiers secours', level: 95, image: 'secours' },
-    { name: 'Travail d\'équipe', level: 85, image: 'travail_equipe' },
-    { name: 'HTML CSS JS', level: 80, image: 'html_css_js' },
-    { name: 'C', level: 65, image: 'c' },
-    { name: 'Git', level: 90, image: 'git' },
-    { name: 'Angular', level: 75, image: 'angular' },
-    { name: 'TypeScript', level: 85, image: 'typescript' },
-    { name: 'API REST', level: 80, image: 'api_rest' }
+    { name: 'Java', level: 80, image: 'java', description: 'Java est le premier langage de programmation que j\'ai touché. \n Appris pour modder mon Minecraft, je l\'utilise aujourd\'hui à l\'université.' },
+    { name: 'Python', level: 90, image: 'python', description: 'Python est surement mon langage de programmation préféré, on peut tout faire avec. Je l\'utilise aujourd\'hui chez Extern-SN' },
+    { name: 'SQL', level: 80, image: 'sql', description: 'Pratique en base de données relationnelle tout le temps. ' },
+    { name: 'JavaEE', level: 75, image: 'javaee', description: 'Appris dans le cadre de projets universitaires, je ne l\'utilise pas autre part' },
+    { name: 'PHP', level: 60, image: 'php', description: 'Utilisé pour la création de sites web dynamiques avant que je connaisse JavaEE.' },
+    { name: 'Premiers secours', level: 95, image: 'secours', description: 'Formation en tant que sapeur-pompier volontaire.' },
+    { name: 'HTML CSS JS', level: 80, image: 'html_css_js', description: 'Utilisé dans le cadre de développement de sites web front-end.' },
+    { name: 'C', level: 65, image: 'c', description: 'Étudié pendant mes cours universitaires de programmation bas niveau.' },
+    { name: 'Git', level: 90, image: 'git', description: 'Utilisé dans la gestion de projets collaboratifs sur GitHub et GitLab.' },
+    { name: 'Angular', level: 75, image: 'angular', description: 'En cours d’apprentissage dans le cadre de mon e-portfolio.' },
+    { name: 'TypeScript', level: 85, image: 'typescript', description: 'Utilisé pour structurer des applications web Vue.js.' },
+    { name: 'API REST', level: 80, image: 'api_rest', description: 'Appris avec JavaEE.' }
   ];
 
-  constructor() {
-    this.skills.sort((a, b) => b.level - a.level);
+  // Ouvrir la modal avec les informations de la compétence sélectionnée
+  openSkillDetails(skill: any) {
+    this.selectedSkill = skill;
   }
 
   getImagePath(imageName: string): string {
